000608508dcdf365b25152731b1d13d96b1f2f7e
DDeShon <ddeshon@live.com>
2021-05-23 22:55:37 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser)
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            print("\n", url)
+
+
+scrape = Scraper("https://news.google.com")
+scrape.scrape()
